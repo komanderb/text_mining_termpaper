1b89930076bf0e7973026888bd3ac73938997e13
komanderb <92434633+komanderb@users.noreply.github.com>
2022-03-10 19:26:58 +0100

    Update code and delete misleading comments

--- code/gutenberg_scrape.R
+++ code/gutenberg_scrape.R
@@ -1,4 +1,4 @@
-install.packages('gutenbergr')
+#install.packages('gutenbergr')
 library(tidyverse)
 library(dplyr)
 library(gutenbergr)
@@ -7,11 +7,6 @@ fairy_id = read.csv("fairy_id.csv")
 fairy_id = fairy_id[-1]
 names(fairy_id)[1] <- 'gutenberg_id' 
 
-gutenberg_metadata %>%
-  filter(gutenberg_bookshelf == "Children's Myths, Fairy Tales, etc.",
-         language == 'en')
-# we could use that but it only returns 14 titles where as there are more than 50 
-# on the webpage but maybe helpful and very straighforward for the others 
 df <- gutenberg_download(as.numeric(fairy_id$gutenberg_id), meta_fields = c("author", "title", "language"))
 
 df <- df %>% filter(language == 'en')
@@ -19,3 +14,14 @@ df <- left_join(df, fairy_id)
 
 write.csv(df, "fairy_tales_en.csv")
 
+df <- gutenberg_metadata %>% 
+  filter(gutenberg_bookshelf == "Children's Literature", language == "en")
+df <- df %>% filter(!(duplicated(title)))
+
+df_big <- gutenberg_download(df$gutenberg_id, meta_fields =  c("author", "title"))
+
+# sometimes books are zipped and thus not downloaded - within the 
+# gutenberg-downloads there are functions that can play around that. 
+
+
+write.csv(df_big, "all_children_en.csv")
